b9e6cb888c864cd9f9b2c8e31af9469dd2a48fdc
99003679 <78849934+99003679@users.noreply.github.com>
2021-02-25 22:20:01 +0530

    Update Cart.cs

--- DemoKartBL/Cart.cs
+++ DemoKartBL/Cart.cs
@@ -9,19 +9,21 @@ namespace DemoKartBL
     public class Cart
     {
        
-        public int Product_id;
-        public String Product_name;
-        public int Product_qty;
-        public decimal Product_price;
+        public int cId;
 
 
-
-        public Cart(String prod_name, int prod_id, int prod_qty, decimal prod_price)
+        public int AddToCart(int ProQuantity)
         {
-            this.Product_name = prod_name;
-            this.Product_id = prod_id;
-            this.Product_qty = prod_qty;
-            this.Product_price = prod_price;
+            if (ProQuantity > 10)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
         }
 
     }
