e27a0343fc040e9951f240aeac499d048a031ee0
99003679 <78849934+99003679@users.noreply.github.com>
2021-02-26 00:00:26 +0530

    Update Program.cs

--- DemoKartMainProgram/Program.cs
+++ DemoKartMainProgram/Program.cs
@@ -9,29 +9,74 @@ namespace DemoKart
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-           
-            List<Product> PList = new List<Product>();
+       
+        static List<Product> CList = new List<Product>(10);
+        static List<Product> PList = new List<Product>(10);
             
+         public void Display(int id)
+        {
+            foreach(Product item in ProductList)
+            {
+                if(id==item.category.categoryid)
+                {
+                    Console.WriteLine("Product id : " + item.Product_id + "\tProduct Name : " + item.Product_name + "\tProduct Price : " + item.Product_price + "\tProduct Quantity : " + item.Product_qty + "\tCategory Id : " + item.category.categorryid + "\tCategory Name : " + item.category.categoryname);
+                }
+            }
+        }
+         static void Main(string[] args)
+        {
+            Category CategoryObj = new Category();
+            Cart CartObj = new Cart();
+            Order OrderObj = new DemoKartBL.Order();
+            Payment paymentObj = new Payment();
+            User UserObj = new User();
 
             Console.Write("Enter the Username: ");
             String Username = Console.ReadLine();
             Console.Write("Enter the Password: ");
             String Password = Console.ReadLine();
-            User a = new User(Username, Password);
-            if (a.Username == "admin" && a.Password == "admin")
+            int value = UserObj.Login(UserName, Password);
+            if (value == 1)
             {
-                Console.WriteLine("login sucessful");
-                Console.WriteLine("Select Category");
-               
+                do
+                {
+                    Console.WriteLine("1.Electronics\t2.Grocery\t3.Clothing\t4.Cosmetics");
+                    int Choice = Convert.ToInt32(Console.ReadLine());
+                    if (Choice == 1)
+                    {
+                        programObj.Display(Choice);
+                        Console.WriteLine("1.Add To Cart\t2.Continue");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        if (num1 == 1)
+                        {
+                            Console.WriteLine("\nEnter ID's of Product You Want Add to Cart : ");
+                            id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("\nEnter How Many Quantity You Want");
+                            int quant = Convert.ToInt32(Console.ReadLine());
+                            foreach (Product item in ProductList)
+                            {
+                                if (id == item.productId)
+                                {
+                                    value = CartObj.AddToCart(quant);
+                                    if (value == 1)
+                                    {
+                                        Console.WriteLine("Quantity Size Exceed The Limit\n");
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        double TotalPric = CategoryObj.CalculatePrice(Choice, item.price, quant);
+                                        TotalPrice1 = TotalPrice1 + TotalPric;
+                                        Product cust_cart = new Product(item.productId, item.productName, item.price, quant, item.CategoryObj.categoriesId, item.CategoryObj.categories);
+                                        CartList.Add(cust_cart);
+                                        Console.WriteLine("Add To cart Is Successfull\n");
+                                    }
 
-                Console.WriteLine("select the payment mode \n 1.cash \n 2.Card");
+                                }
+                            }
 
-
-
-
-            }
+                        }
+                    }
         }
     }
 }
